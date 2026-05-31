26b249b92151f1992de8c45829c888161037334a
MuhammadFikri <fikrimuhammad2007@gmail.com>
2026-05-31 11:27:00 +0700

    add: add and delete manga

--- src/algorithms/actions/add.ts
+++ src/algorithms/actions/add.ts
@@ -1,43 +1,75 @@
 import type { Manga } from "../../models/manga";
-import { input, select, checkbox } from "@inquirer/prompts";
+import { input } from "@inquirer/prompts";
+import chalk from "chalk";
+import { writeFileSync } from "fs";
 import { Genre } from "../../models/genre";
 import { kmp } from "../string-matching";
 import { join, dirname } from 'path'
 import { fileURLToPath } from 'url'
 import checkboxPlusPrompt from "inquirer-checkbox-plus-plus";
+import { nanoid } from "nanoid";
 
 
 const currentFile = fileURLToPath(import.meta.url)
 const src = dirname(currentFile)
 
 export async function add(mangas: Manga[]){
-    const title = await input({ message: 'Masukan judul manga: ', theme: { prefix: '>' } })
-    const selectedGenres = await checkboxPlusPrompt({
-        message: 'Pilih Genre',
-        searchable: true,
-        highlight: true,
-        source: function(answer, input){
-            input = input || '';
-
-            return new Promise(function(resolve){
-                const genres = Object.values(Genre).map((genre) => ({
-                    name: genre,
-                    value: genre,
-                }))
-
-                if(!input) {
-                    resolve(genres)
-                    return
-                }
-
-                const data = genres.filter((genre) => 
-                    kmp(input, genre.name).length > 0
-                )
-                resolve(data)
-            })
+    while(true){
+        const title = await input({ message: 'Masukan judul manga: ', theme: { prefix: '>' } })
+        const isExit = mangas.some((manga)=> manga.title.toLocaleLowerCase() === title.toLowerCase())
+        if(isExit) {
+            console.log(
+                chalk.red.bold(`\n✖ Manga dengan judul `) +
+                chalk.gray.bold(title.toLowerCase()) +
+                chalk.red.bold(` sudah ada.`)
+            );
+            return
         }
-    })
-    const tagsInput = await input({ message: 'Masukan tags (pisah koma): ' })
-    const tags = tagsInput.split(',').map(t => t.trim()).filter(t => t !== "")
-    
+
+        const author = await input({ message: 'Masukan nama penulis (author): ', theme: { prefix: '>' } });
+        const selectedGenres = await checkboxPlusPrompt({
+            message: 'Pilih Genre',
+            searchable: true,
+            highlight: true,
+            source: function(answer, input){
+                input = input || '';
+
+                return new Promise(function(resolve){
+                    const genres = Object.values(Genre).map((genre) => ({
+                        name: genre,
+                        value: genre,
+                    }))
+
+                    if(!input) {
+                        resolve(genres)
+                        return
+                    }
+
+                    const data = genres.filter((genre) => 
+                        kmp(input, genre.name).length > 0
+                    )
+                    resolve(data)
+                })
+            }
+        })
+        const tagsInput = await input({ message: 'Masukan tags (pisah koma): ' })
+        const tags = tagsInput.split(',').map(t => t.trim()).filter(t => t !== "")
+
+        const newManga: Manga = {
+            id: nanoid(),
+            title: title,
+            author: author,
+            genre: selectedGenres as Genre[], 
+            tags: tags
+        };
+
+        mangas.push(newManga)
+        try {
+            const pathToJson = join(src, '..', '..', 'data', 'mangas.json'); 
+            writeFileSync(pathToJson, JSON.stringify(mangas, null, 4), 'utf-8');
+            console.log(`\n> Data manga "${newManga.title}" berhasil disimpan!`);
+        } catch (error) {
+            console.error("\n> Gagal menyimpan data:", error);
+        }
+    }   
 }
--- src/algorithms/actions/delete.ts
+++ src/algorithms/actions/delete.ts
@@ -1,4 +1,8 @@
 import type { Manga } from "../../models/manga"
+import { input, select } from "@inquirer/prompts";
+import chalk from "chalk";
+import { searchMangaTitleCandidates } from "../searching";
+import { writeFileSync } from "fs"
 import { join, dirname } from 'path'
 import { fileURLToPath } from 'url'
 
@@ -6,6 +10,48 @@ import { fileURLToPath } from 'url'
 const currentFile = fileURLToPath(import.meta.url)
 const src = dirname(currentFile)
 
-export function del(mangas: Manga[]){
-    
+export async function del(mangas: Manga[]){
+    const query = await input({ 
+        message: 'Masukan judul manga yang ingin dihapus: ', 
+        theme: { prefix: '>' } 
+    });
+    const candidates = query ? searchMangaTitleCandidates(query, mangas) : []
+
+    if(candidates.length === 0){
+        console.log(chalk.red.bold('\n✖ Manga tidak ditemukan.'))
+        console.log(chalk.dim('Coba gunakan kata kunci lain!\n'))
+        
+        return
+    }
+
+    console.log(chalk.green.bold(`\nDitemukan ${candidates.length} manga.`))
+
+    const titleToDelete = await select({
+        message: '',
+        choices: candidates.map((title) => ({
+            name: title,
+            value: title,
+        })),
+        theme: {
+            prefix: chalk.dim('Maksudnya ini?')
+        }
+    })
+
+
+    const index = mangas.findIndex((manga) => manga.title.toLowerCase() === titleToDelete.toLowerCase());
+
+    if (index !== -1) {
+        const deletedManga = mangas.splice(index, 1)[0];
+
+        try {
+            const pathToJson = join(src, '..', '..', 'data', 'mangas.json'); 
+            writeFileSync(pathToJson, JSON.stringify(mangas, null, 4), 'utf-8');
+            
+            console.log(`\n> Data manga "${deletedManga.title}" berhasil dihapus!`);
+        } catch (error) {
+            console.error("\n> Gagal menyimpan penghapusan data:", error);
+        }
+    } else {
+        console.log(`\n> Manga dengan judul "${titleToDelete}" tidak ditemukan.`);
+    }
 }
